3c575c4e1045a03a316bf92d49b2655ccef4745c
Kenja <estebanxa@outlook.com>
2026-03-15 10:52:35 -0600

    Semana 2 commint 9 Eliminacion,  edicion y modifiacion de menus

--- Controllers/FoodController.cs
+++ Controllers/FoodController.cs
@@ -1,123 +1,27 @@
-using System;
 using System.Collections.Generic;
-using System.Windows.Forms;
 using NutritionApp.Data;
 using NutritionApp.Models;
-using Menu = NutritionApp.Models.Menu;
-using MenuItem = NutritionApp.Models.MenuItem;
 
 namespace NutritionApp.Controllers
 {
-    public class MenuController
+    public class FoodController
     {
-        private MenuRepository repo = new MenuRepository();
+        private FoodRepository repo = new FoodRepository();
 
-        public List<Menu> GetMenusByUser(int userId)
+        public List<FoodProduct> GetAllFoods()
         {
-            try
-            {
-                List<Menu> all = repo.GetAll();
-                List<Menu> result = new List<Menu>();
-                foreach (Menu m in all)
-                {
-                    if (m.UserId == userId)
-                        result.Add(m);
-                }
-                return result;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error loading menus: " + ex.Message);
-                return new List<Menu>();
-            }
+            return repo.GetAll();
         }
 
-        public void CreateMenu(Menu menu)
+        public FoodProduct GetById(int id)
         {
-            try
+            List<FoodProduct> foods = repo.GetAll();
+            foreach (FoodProduct f in foods)
             {
-                List<Menu> all = repo.GetAll();
-
-                int maxId = 0;
-                foreach (Menu m in all)
-                {
-                    if (m.Id > maxId)
-                        maxId = m.Id;
-                }
-                menu.Id = maxId + 1;
-
-                CalculateTotals(menu);
-                all.Add(menu);
-                repo.SaveAll(all);
+                if (f.Id == id)
+                    return f;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error creating menu: " + ex.Message);
-            }
-        }
-
-        public void UpdateMenu(Menu updatedMenu)
-        {
-            try
-            {
-                List<Menu> all = repo.GetAll();
-                CalculateTotals(updatedMenu);
-                for (int i = 0; i < all.Count; i++)
-                {
-                    if (all[i].Id == updatedMenu.Id)
-                    {
-                        all[i] = updatedMenu;
-                        break;
-                    }
-                }
-                repo.SaveAll(all);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error updating menu: " + ex.Message);
-            }
-        }
-
-        public void DeleteMenu(int menuId)
-        {
-            try
-            {
-                List<Menu> all = repo.GetAll();
-                Menu toRemove = null;
-                foreach (Menu m in all)
-                {
-                    if (m.Id == menuId)
-                    {
-                        toRemove = m;
-                        break;
-                    }
-                }
-                if (toRemove != null)
-                {
-                    all.Remove(toRemove);
-                    repo.SaveAll(all);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error deleting menu: " + ex.Message);
-            }
-        }
-
-        public void CalculateTotals(Menu menu)
-        {
-            menu.TotalCalories = 0;
-            menu.TotalProtein = 0;
-            menu.TotalCarbohydrates = 0;
-            menu.TotalFat = 0;
-
-            foreach (MenuItem item in menu.Items)
-            {
-                menu.TotalCalories += item.Calories;
-                menu.TotalProtein += item.Protein;
-                menu.TotalCarbohydrates += item.Carbohydrates;
-                menu.TotalFat += item.Fat;
-            }
+            return null;
         }
     }
 }
--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,27 +1,123 @@
+using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using NutritionApp.Data;
 using NutritionApp.Models;
+using Menu = NutritionApp.Models.Menu;
+using MenuItem = NutritionApp.Models.MenuItem;
 
 namespace NutritionApp.Controllers
 {
-    public class FoodController
+    public class MenuController
     {
-        private FoodRepository repo = new FoodRepository();
+        private MenuRepository repo = new MenuRepository();
 
-        public List<FoodProduct> GetAllFoods()
+        public List<Menu> GetMenusByUser(int userId)
         {
-            return repo.GetAll();
+            try
+            {
+                List<Menu> all = repo.GetAll();
+                List<Menu> result = new List<Menu>();
+                foreach (Menu m in all)
+                {
+                    if (m.UserId == userId)
+                        result.Add(m);
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading menus: " + ex.Message);
+                return new List<Menu>();
+            }
         }
 
-        public FoodProduct GetById(int id)
+        public void CreateMenu(Menu menu)
         {
-            List<FoodProduct> foods = repo.GetAll();
-            foreach (FoodProduct f in foods)
+            try
             {
-                if (f.Id == id)
-                    return f;
+                List<Menu> all = repo.GetAll();
+
+                int maxId = 0;
+                foreach (Menu m in all)
+                {
+                    if (m.Id > maxId)
+                        maxId = m.Id;
+                }
+                menu.Id = maxId + 1;
+
+                CalculateTotals(menu);
+                all.Add(menu);
+                repo.SaveAll(all);
             }
-            return null;
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating menu: " + ex.Message);
+            }
+        }
+
+        public void UpdateMenu(Menu updatedMenu)
+        {
+            try
+            {
+                List<Menu> all = repo.GetAll();
+                CalculateTotals(updatedMenu);
+                for (int i = 0; i < all.Count; i++)
+                {
+                    if (all[i].Id == updatedMenu.Id)
+                    {
+                        all[i] = updatedMenu;
+                        break;
+                    }
+                }
+                repo.SaveAll(all);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating menu: " + ex.Message);
+            }
+        }
+
+        public void DeleteMenu(int menuId)
+        {
+            try
+            {
+                List<Menu> all = repo.GetAll();
+                Menu toRemove = null;
+                foreach (Menu m in all)
+                {
+                    if (m.Id == menuId)
+                    {
+                        toRemove = m;
+                        break;
+                    }
+                }
+                if (toRemove != null)
+                {
+                    all.Remove(toRemove);
+                    repo.SaveAll(all);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting menu: " + ex.Message);
+            }
+        }
+
+        public void CalculateTotals(Menu menu)
+        {
+            menu.TotalCalories = 0;
+            menu.TotalProtein = 0;
+            menu.TotalCarbohydrates = 0;
+            menu.TotalFat = 0;
+
+            foreach (MenuItem item in menu.Items)
+            {
+                menu.TotalCalories += item.Calories;
+                menu.TotalProtein += item.Protein;
+                menu.TotalCarbohydrates += item.Carbohydrates;
+                menu.TotalFat += item.Fat;
+            }
         }
     }
 }
